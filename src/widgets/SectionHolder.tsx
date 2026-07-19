18332e6e2d54f8d780367703f88834f687f0101a
Mor9ak <skricheery0rik@gmail.com>
2026-07-19 21:27:09 +0300

    style(section holder): - change placement - change form of sections

--- src/widgets/SectionHolder.tsx
+++ src/widgets/SectionHolder.tsx
@@ -9,50 +9,54 @@ import IndexDecoderMech from "../features/IndexDecoderMech";
 
 const SectionHolder = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 gap-x-13 justify-start items-center uniWidth">
-            <Section
-                label={"Caesar"}
-                alphabet={"abcdefghijklmnopqrstuvwxyz"}
-                link = {"https://ru.wikipedia.org/wiki/%D0%A8%D0%B8%D1%84%D1%80_%D0%A6%D0%B5%D0%B7%D0%B0%D1%80%D1%8F"}
-                encrypting={CaesarMech}
-                inputPlaceholder1={"Enter text"}
-                inputPlaceholder2={"Enter alphabet (english rn))"}
-                inputPlaceholder3={"Enter step"}
-            />
-            <Section
-                label={"Mirror"}
-                encrypting={MirrorMech}
-                inputPlaceholder1={"Enter text"}
-            />
-            <Section
-                label={"Index"}
-                link={"https://ru.wikipedia.org/wiki/%D0%90%D1%84%D1%84%D0%B8%D0%BD%D0%BD%D1%8B%D0%B9_%D1%88%D0%B8%D1%84%D1%80"}
-                encrypting={IndexMech}
-                alphabet={"abcdefghijklmnopqrstuvwxyz"}
-                inputPlaceholder1={"Enter text"}
-                inputPlaceholder2={"Enter alphabet (english rn))"}
-            />
-            <Section
-                label={"Atbash"}
-                link={"https://ru.wikipedia.org/wiki/%D0%90%D1%82%D0%B1%D0%B0%D1%88"}
-                encrypting={AtbashMech}
-                alphabet={"abcdefghijklmnopqrstuvwxyz"}
-                inputPlaceholder1={"Enter text"}
-                inputPlaceholder2={"Enter alphabet (english rn))"}
-            />
-            <Section
-                label={"One to two"}
-                link={"https://ru.wikipedia.org/wiki/%D0%9F%D0%B5%D1%80%D0%B5%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BE%D1%87%D0%BD%D1%8B%D0%B9_%D1%88%D0%B8%D1%84%D1%80"}
-                encrypting={OneTwoMech}
-                inputPlaceholder1={"Enter text"}
-            />
-            <Section
-                label={"Index decoder"}
-                standardText={"19042319"}
-                encrypting={IndexDecoderMech}
-                inputPlaceholder1={"Enter text"}
-                inputPlaceholder2={"Enter alphabet (english rn))"}
-            />
+        <div className={"flex flex-col md:flex-row gap-3 items-center uniWidth"}>
+            <div className={"flex flex-col gap-3 *:w-full *:items-center"}>
+                <Section
+                    label={"Caesar"}
+                    alphabet={"abcdefghijklmnopqrstuvwxyz"}
+                    link = {"https://ru.wikipedia.org/wiki/%D0%A8%D0%B8%D1%84%D1%80_%D0%A6%D0%B5%D0%B7%D0%B0%D1%80%D1%8F"}
+                    encrypting={CaesarMech}
+                    inputPlaceholder1={"Enter text"}
+                    inputPlaceholder2={"Enter alphabet (english rn))"}
+                    inputPlaceholder3={"Enter step"}
+                />
+                <Section
+                    label={"Index"}
+                    link={"https://ru.wikipedia.org/wiki/%D0%90%D1%84%D1%84%D0%B8%D0%BD%D0%BD%D1%8B%D0%B9_%D1%88%D0%B8%D1%84%D1%80"}
+                    encrypting={IndexMech}
+                    alphabet={"abcdefghijklmnopqrstuvwxyz"}
+                    inputPlaceholder1={"Enter text"}
+                    inputPlaceholder2={"Enter alphabet (english rn))"}
+                />
+                <Section
+                    label={"Atbash"}
+                    link={"https://ru.wikipedia.org/wiki/%D0%90%D1%82%D0%B1%D0%B0%D1%88"}
+                    encrypting={AtbashMech}
+                    alphabet={"abcdefghijklmnopqrstuvwxyz"}
+                    inputPlaceholder1={"Enter text"}
+                    inputPlaceholder2={"Enter alphabet (english rn))"}
+                />
+            </div>
+            <div className={"flex flex-col gap-3 *:w-full *:items-center"}>
+                <Section
+                    label={"Mirror"}
+                    encrypting={MirrorMech}
+                    inputPlaceholder1={"Enter text"}
+                />
+                <Section
+                    label={"One to two"}
+                    link={"https://ru.wikipedia.org/wiki/%D0%9F%D0%B5%D1%80%D0%B5%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BE%D1%87%D0%BD%D1%8B%D0%B9_%D1%88%D0%B8%D1%84%D1%80"}
+                    encrypting={OneTwoMech}
+                    inputPlaceholder1={"Enter text"}
+                />
+                <Section
+                    label={"Index decoder"}
+                    standardText={"19042319"}
+                    encrypting={IndexDecoderMech}
+                    inputPlaceholder1={"Enter text"}
+                    inputPlaceholder2={"Enter alphabet (english rn))"}
+                />
+            </div>
         </div>
     );
 }
