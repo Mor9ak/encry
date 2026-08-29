5efd958e3917391b1a714c2e891071d2ccd7912b
Mor9ak <skricheery0rik@gmail.com>
2026-08-29 21:26:43 +0300

    perf(widgets): add React.memo to Header and update ThemeChanger import
    - Wrap Header with React.memo to prevent parent re-renders cascade
    - Update ThemeChanger import path to feature-sliced location
    - Add displayName for React DevTools debugging

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import {ThemeChanger} from "../features/theme/themeChanger";
 
-const Header = () => {
+const Header = React.memo(() => {
     return (
-        <header className="relative bg-elementsBg py-3 rounded-lg !mt-7 uniWidth">
+        <header className="relative bg-elementsBg py-3 rounded-lg mt-7 uniWidth">
             <div className="absolute top-3 right-3">
-                <ThemeChanger />
+                <ThemeChanger/>
             </div>
 
             <div className="flex flex-col items-center justify-center">
-                <h1 className="text-4xl text-center text-mainText">
-                    <a href="/">Encry</a>
-                </h1>
+                <h1 className="text-4xl text-center text-mainText">Encry</h1>
                 <p className="text-center text-xl text-secondaryText mt-2">
                     Ciphers playgrounds by Mor9ak (Kazakou Yaroslau)
                 </p>
             </div>
         </header>
     );
-};
+});
+
+Header.displayName = 'Header';
 
 export default Header;
